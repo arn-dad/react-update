6da8d48578924398a1e6eaf5be56590928e1d09c
arnold <dadayanaa@yahoo.com>
2019-12-06 16:33:17 +0400

    final useReducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {
+        count: state.count + 1
+      }
+    case 'decrement':
+        return {
+          count: state.count - 1
+      }  
+    case 'reset':
+        return initialState
+    default:
+      return initialState;
+      break;
+  }
+}
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, initialState)
     return (
       <div>
-        Empty
+        Count: <b>{state.count}</b>
+        <br/>
+        <button type="button" className="btn btn-info" onClick={() => { dispatch({ type: 'increment' }) }} >Increment</button>
+        <button className="btn btn-danger" onClick={() => { dispatch({ type: 'decrement' }) }} >Decrement</button>
+        <button className="btn btn-dark" onClick={() => { dispatch({ type: 'reset' }) }} >Reset</button>
       </div>
-    );
-  }
+  );
 }
 
 export default App;
