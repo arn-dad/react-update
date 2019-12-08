ca28f20bb16e6403edd3df93019bb21e763ed76e
ArnodDadayan <dadayanaa@yahoo.com>
2019-12-08 14:17:23 +0400

    Updates

---
+++ precache-manifest.8d72beae68f85887c2c3586654c792ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704a010fe7716259b989ceeccae00e35",
+    "url": "/react-update/index.html"
+  },
+  {
+    "revision": "9d3a2856dba1f5813671",
+    "url": "/react-update/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2d0aa541571761e54ca6",
+    "url": "/react-update/static/css/main.3eb880e8.chunk.css"
+  },
+  {
+    "revision": "9d3a2856dba1f5813671",
+    "url": "/react-update/static/js/2.9b313086.chunk.js"
+  },
+  {
+    "revision": "2d0aa541571761e54ca6",
+    "url": "/react-update/static/js/main.8fd41355.chunk.js"
+  },
+  {
+    "revision": "b7ac063affad53f6bef0",
+    "url": "/react-update/static/js/runtime-main.ea45aaee.js"
+  }
+]);
